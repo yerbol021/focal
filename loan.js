4096d9dae4a005fbf2f05b6d8143d46b2300fce7
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-29 22:54:20 -0700

    some update

---
+++ loan.js
@@ -0,0 +1,10 @@
+let creditLimit = 5000;
+
+/*
+ * Input: number of dollars to loan out
+ * Returns: Promise of loan which may or may not fulfill, based on remaining credit. 
+ * If creditLimit is less than the requested amount, only the remaining limit is loaned out, otherwise the full amount is loaned out. If $0 remain in the limit, the loan request is rejected (error!).
+ */
+const loanOut = function(amount) {
+
+};
